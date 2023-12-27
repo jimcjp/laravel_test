7af7b5cad8b35a398d404b58ec3ecf489c177993
jimcjp <jimcjpkg9@gmail.com>
2023-12-27 16:22:56 +0800

    first commit

--- resources/views/layout/style.blade.php
+++ resources/views/layout/style.blade.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/common.css') }}">
-<link rel="stylesheet" href="{{ asset('css/sweetalert2.css') }}">
+<link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ secure_asset('css/common.css') }}">
+<link rel="stylesheet" href="{{ secure_asset('css/sweetalert2.css') }}">
 <style>
 
 </style>
