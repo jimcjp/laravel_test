1ad6bddd8ec099738b9e1032ef15280c0db7dfed
jimcjp <jimcjpkg9@gmail.com>
2023-12-27 17:23:51 +0800

    first commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 // use App\Models\User;
 
 // use Illuminate\Support\Facades\Auth;
-// use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 // use Illuminate\Support\Facades\Storage;
 
 
