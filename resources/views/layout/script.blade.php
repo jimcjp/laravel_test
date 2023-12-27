893b25482bc3303883e1e6fde7c61e07c937cfb4
jimcjp <jimcjpkg9@gmail.com>
2023-12-27 15:32:09 +0800

    first commit

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1,8 +1,8 @@
 {{-- <script src="{{ asset('js/jquery.slim.min.js') }}"></script> --}}
-<script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
-<script src="{{ asset('js/popper.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/sweetalert2.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.10.1/sweetalert2.min.js"></script>
 <script>
 
     
